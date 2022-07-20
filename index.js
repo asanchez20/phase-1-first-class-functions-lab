226721d923eedf098353b5a48e7958214bd5301c
Alvin Sanchez <alvin.sanchez27@gmail.com>
2022-07-19 21:48:26 -0400

    commit tree f5f7d137acb6e563f05ba33f2bfd93becfb33d08

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const names = (['Antonia', 'Nuru', 'Amari', 'Mo'])
+const returnFirstTwoDrivers = function(names) {
+    return names.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (names) {
+    return names.slice(-2)
+}
+
+const selectingDrivers = ([returnFirstTwoDrivers,returnLastTwoDrivers])
+
+function createFareMultiplier(number){
+    return function (times) {
+      return  number * times
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(names, drivers){
+    return drivers(names)
+}
